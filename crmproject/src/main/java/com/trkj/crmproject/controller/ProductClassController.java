861210c0e0e214271b0499af469b39dd14a8c44c
wentaoovo <980464050@qq.com>
2021-11-07 08:56:59 +0800

    何文韬第一次提交

--- crmproject/src/main/java/com/trkj/crmproject/controller/ProductClassController.java
+++ crmproject/src/main/java/com/trkj/crmproject/controller/ProductClassController.java
@@ -1,9 +1,13 @@
 package com.trkj.crmproject.controller;
+import com.trkj.crmproject.entity.ProductClass;
+import com.trkj.crmproject.service.ProductClassService;
+import com.trkj.crmproject.util.MyResult;
+import com.trkj.crmproject.util.ResultVoUtil;
+import com.trkj.crmproject.vo.ResultVo;
+import org.springframework.web.bind.annotation.*;
 
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +20,51 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/product-class")
 public class ProductClassController {
+    @Resource
+    private ProductClassService productClassService;
+    /**
+     * 新增产品类别
+     * @param productClass
+     * @return
+     */
+    @PostMapping("/add")
+    public MyResult add(@RequestBody ProductClass productClass){
+            return MyResult.SUCCESS_Object(productClassService.save(productClass));
+    }
+    /**
+     * 根据id删除产品类别
+     * @return
+     */
+    @DeleteMapping("/delete/{id}")
+    public MyResult delete(@PathVariable("id") int id){
+        return MyResult.SUCCESS_Object(productClassService.removeById(id));
+    }
 
+    /**
+     * 根据id修改产品类别
+     * @return
+     */
+    @PutMapping("/update/{id}")
+    public MyResult update(@PathVariable("id") int id, @RequestBody ProductClass productClass){
+        productClass.setPcId(id);
+        return MyResult.SUCCESS_Object(productClassService.saveOrUpdate(productClass));
+    }
+    /**
+     * 根据id查询产品类别
+     * @return
+     */
+    @GetMapping("find/{id}")
+    public MyResult find(@PathVariable("id") String id){
+        ProductClass productClass = productClassService.getById(id);
+        return MyResult.SUCCESS_Object(productClass);
+    }
+    /**
+     * 查询全部产品类别
+     * @return
+     */
+    @GetMapping("findAll")
+    public ResultVo findAll(){
+        List<ProductClass> all = productClassService.list();
+        return ResultVoUtil.success(all);
+    }
 }
--- crmproject/src/main/java/com/trkj/crmproject/service/impl/ProductServiceImpl.java
+++ crmproject/src/main/java/com/trkj/crmproject/service/impl/ProductServiceImpl.java
@@ -1,10 +1,17 @@
 package com.trkj.crmproject.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.trkj.crmproject.entity.Product;
 import com.trkj.crmproject.dao.ProductDao;
 import com.trkj.crmproject.service.ProductService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.trkj.crmproject.vo.SearchListVo;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +23,31 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ProductServiceImpl extends ServiceImpl<ProductDao, Product> implements ProductService {
+    @Resource
+    private ProductDao productDao;
+    @Override
+    public IPage<Product> findList(SearchListVo vo) {
+        if (vo.getPageNum()==null){
+            vo.setPageNum(1);
+        }
+        if (vo.getPageSize()==null){
+            vo.setPageSize(10);
+        }
+        Page<Product> page=new Page<>(vo.getPageNum(),vo.getPageSize());
+        QueryWrapper<Product> wrapper=new QueryWrapper<>();
+        if (vo.getKeyword()!=null){
+            wrapper.like("CONCAT(pr_coding,pr_name,pr_model)",vo.getKeyword());
+        }
+        wrapper.eq("pr_timeliness",1);
+        IPage<Product> productIPage = productDao.selectList(page, wrapper);
+        return productIPage;
+    }
 
+    @Override
+    public List<Product> findAll() {
+        QueryWrapper<Product> wrapper =new QueryWrapper<>();
+        wrapper.eq("pr_state",0);
+        List<Product> list = list(wrapper);
+        return list;
+    }
 }
